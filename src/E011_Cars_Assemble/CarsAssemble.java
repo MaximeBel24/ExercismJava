bd00c429946af6c6ae28c2a0b5edbef9d396e42c
Maxime <maxime.b2494@gmail.com>
2024-08-20 00:03:31 +0200

    E012 done

--- src/E011_Cars_Assemble/CarsAssemble.java
+++ src/E011_Cars_Assemble/CarsAssemble.java
@@ -1,4 +1,21 @@
 package E011_Cars_Assemble;
 
 public class CarsAssemble {
+    public double productionRatePerHour(int speed) {
+        double successRate = 1;
+
+        if(speed >= 5 && speed <= 8) {
+            successRate = 0.9;
+        } else if (speed == 9){
+            successRate = 0.8;
+        } else if (speed >= 10) {
+            successRate = 0.77;
+        }
+
+        return (speed * 221) * successRate;
+    }
+    public int workingItemsPerMinute(int speed) {
+        double items = productionRatePerHour(speed) / 60;
+        return (int) items;
+    }
 }
